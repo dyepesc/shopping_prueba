2f7112e7808485cd6ba4860bed389f90347e7e6c
Daniel Yepes <danielyepes@Daniels-MacBook-Pro.local>
2023-08-31 23:32:16 -0700

    Se crea API con fake API (Si funciona)

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,19 +6,23 @@ function Home () {
   const [items, setItems] = useState(null)
 
   useEffect(() => {
-    fetch('https://api.escuelajs.co/api/v1/categories')
+    fetch('https://fakestoreapi.com/products')
     .then(response => response.json())
     .then(data => setItems(data))
+    .then(json=>console.log(json))
   }, [])
   return (
 
       <Layout>
         Home
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
         {
           items?.map(item => (
             <Card key={item.id} data={item} />
           ))
         }
+        </div>
+       
       </Layout>
   )
 }
