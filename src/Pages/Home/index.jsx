8e487c63b53a477ec09e943bb020196e9bccb632
Daniel Yepes <danielyepes@Daniels-MacBook-Pro.local>
2023-08-31 22:19:09 -0700

    Integracion con API de Platzi (No funcina)

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect} from "react"
 import Layout from "../../Components/Layout"
 import Card from '../../Components/Card'
 
 function Home () {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/categories')
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
   return (
 
       <Layout>
         Home
-        <Card />
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
       </Layout>
   )
 }
